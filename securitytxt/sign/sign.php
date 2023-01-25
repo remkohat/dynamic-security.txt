2dbad4c7d48d32946670c0c7c9752f3ce72e76ff
Remko <94494764+remkohat@users.noreply.github.com>
2023-01-25 21:27:51 +0100

    Syntax simplified

--- securitytxt/sign/sign.php
+++ securitytxt/sign/sign.php
@@ -1,18 +1,14 @@
 <?php
 
-if (isset($publickeyfile)) {
-    if (!empty($publickeyfile)) {
-        if (file_exists($publickeyfile)) {
-            $gpg = new gnupg();
-            $gpg->seterrormode(GNUPG_ERROR_WARNING);
-            $gpg->setsignmode(GNUPG_SIG_MODE_CLEAR);
-            $publicdata = file_get_contents($publickeyfile);
-            $publickey = $gpg->import($publicdata);
-            $gpg->addsignkey($publickey['fingerprint']);
-            $signed = $gpg->sign($securitytxt);
-            $securitytxt = $signed;
-        }
-    }
+if (isset($publickeyfile) && !empty($publickeyfile) && file_exists($publickeyfile)) {
+    $gpg = new gnupg();
+    $gpg->seterrormode(GNUPG_ERROR_WARNING);
+    $gpg->setsignmode(GNUPG_SIG_MODE_CLEAR);
+    $publicdata = file_get_contents($publickeyfile);
+    $publickey = $gpg->import($publicdata);
+    $gpg->addsignkey($publickey['fingerprint']);
+    $signed = $gpg->sign($securitytxt);
+    $securitytxt = $signed;
 }
 
 ?>
