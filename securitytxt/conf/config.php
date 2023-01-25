88ee371d2810e76b38da9a8a7495af3d0f494937
Remko <94494764+remkohat@users.noreply.github.com>
2023-01-25 14:43:21 +0100

    Config file

---
+++ securitytxt/conf/config.php
@@ -0,0 +1,57 @@
+<?php
+
+# Contact: A link or e-mail address for people to contact you about security issues.
+# Remember to include "https://" for URLs, and "mailto:" for e-mails.
+#       Example: $contact    = "https://domain.tld/contact";
+#       Can have more than one, i.e.
+#               $contact[0] = "mailto:security@domain.tld";
+#               $contact[1] = "mailto:security[at]domain[dot]tld";
+#
+$contact = "";
+
+# Policy: A link to a policy detailing what security researchers should do when searching for or reporting security issues.
+# Remember to include "https://".
+#       Example: $policy = "https://domain.tld/policy";
+#       Can have more than one, i.e.
+#               $policy[0] = "https://domain.tld/policy";
+#               $policy[1] = "https://policy.domain.tld";
+#
+$policy = "";
+
+# Acknowledgements: A link to a web page where you say thank you to security researchers who have helped you.
+# Remember to include "https://".
+#       Example: $acknowledgments = "https://domain.tld/acknowledgements";
+#       Can have more than one, i.e.
+#               $acknowledgments[0] = "https://domain.tld/acknowledgements";
+#               $acknowledgments[1] = "https://domain.tld/contributors";
+#
+$acknowledgments = "";
+
+# Hiring: A link to any security-related job openings in your organisation.
+# Remember to include "https://".
+#       Example: $hiring = "https://domain.tld/jobs";
+#       Can have more than one, i.e.
+#               $hiring[0] = "https://domain.tld/jobs";
+#               $hiring[1] = "https://jobs.domain.tld";
+#
+$hiring = "";
+
+# Preferred-Languages: A comma-separated list of language codes that your security team speaks. You may include more than one language.
+#
+$lang = "en";
+
+# Encryption: A link to a key which security researchers should use to securely talk to you.
+# Remember to include "https://".
+#       Example: $encryption = "https://domain.tld/encryption";
+#       Can have more than one, i.e.
+#               $encryption[0] = "https://domain.tld/encryption";
+#               $encryption[1] = "dns:5d2d37ab76d47d36._openpgpkey.domain.tld?type=OPENPGPKEY";
+#
+$encryption[0] = "";
+
+# Signing: A location of the public key to sign security.txt with.
+# If not specified, invalid or empty than security.txt will not be signed.
+#
+$publickeyfile = "";
+
+?>
