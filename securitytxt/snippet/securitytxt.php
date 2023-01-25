55dce722644c93f120098e9d2330cf8371a280c7
Remko <94494764+remkohat@users.noreply.github.com>
2023-01-25 20:50:29 +0100

    Syntax and bugfixes

--- securitytxt/snippet/securitytxt.php
+++ securitytxt/snippet/securitytxt.php
@@ -6,57 +6,41 @@ $expires = gmdate("Y-m-d\TH:i:s\Z", strtotime("+1 year"));
 
 $canonicaltxt = "# Canonical URL\nCanonical: https://$host$uri\n\n";
 
-if (isset($contact)) {
-    if (!empty($contact)) {
-        if (is_array($contact)) {
-            $contacttxt = "# Our security address\nContact: " . implode("\nContact: ", $contact) . "\n\n";
-        } else {
-            $contacttxt = "# Our OpenPGP key\nEncryption: $contact\n\n";
-        }
+if (isset($contact) && !empty($contact)) {
+    if (is_array($contact)) {
+        $contacttxt = "# Our security address\nContact: " . implode("\nContact: ", $contact) . "\n\n";
     } else {
-        $contacttxt = "";
+        $contacttxt = "# Our security address\nContact: $contact\n\n";
     }
 } else {
     $contacttxt = "";
 }
 
-if (isset($policy)) {
-    if (!empty($policy)) {
-        if (is_array($policy)) {
-            $policytxt = "# Our security policy\nPolicy: " . implode("\nPolicy: ",$policy) . "\n\n";
-        } else {
-            $policytxt = "# Our security policy\nPolicy: $policy\n\n";
-        }
+if (isset($policy) && !empty($policy)) {
+    if (is_array($policy)) {
+        $policytxt = "# Our security policy\nPolicy: " . implode("\nPolicy: ",$policy) . "\n\n";
     } else {
-        $policytxt = "";
+        $policytxt = "# Our security policy\nPolicy: $policy\n\n";
     }
 } else {
     $policytxt = "";
 }
 
-if (isset($acknowledgments)) {
-    if (!empty($acknowledgments)) {
-        if (is_array($acknowledgments)) {
-            $acknowledgmentstxt = "# Hall of fame\nAcknowledgments: " . implode("\nAcknowledgments: ", $acknowledgments) . "\n\n";
-        } else {
-            $acknowledgmentstxt = "# Hall of fame\nAcknowledgments: $acknowledgments\n\n";
-        }
+if (isset($acknowledgments) && !empty($acknowledgments)) {
+    if (is_array($acknowledgments)) {
+        $acknowledgmentstxt = "# Hall of fame\nAcknowledgments: " . implode("\nAcknowledgments: ", $acknowledgments) . "\n\n";
     } else {
-        $acknowledgmentstxt = "";
+        $acknowledgmentstxt = "# Hall of fame\nAcknowledgments: $acknowledgments\n\n";
     }
 } else {
     $acknowledgmentstxt = "";
 }
 
-if (isset($hiring)) {
-    if (!empty($hiring)) {
-        if (is_array($hiring)) {
-            $hiringtxt = "# Jobs for you\nHiring: " . implode("\nHiring: ",$hiring) . "\n\n";
-        } else {
-            $hiringtxt = "# Jobs for you\nHiring: $hiring\n\n";
-        }
+if (isset($hiring) && !empty($hiring)) {
+    if (is_array($hiring)) {
+        $hiringtxt = "# Jobs for you\nHiring: " . implode("\nHiring: ",$hiring) . "\n\n";
     } else {
-        $hiringtxt = "";
+        $hiringtxt = "# Jobs for you\nHiring: $hiring\n\n";
     }
 } else {
     $hiringtxt = "";
@@ -64,15 +48,11 @@ if (isset($hiring)) {
 
 $langtxt = "# These are the languages we speak\nPreferred-Languages: $lang\n\n";
 
-if (isset($encryption)) {
-    if (!empty($encryption)) {
-        if (is_array($encryption)){
-            $encryptiontxt = "# Our OpenPGP key\nEncryption: " . implode("\nEncryption: ", $encryption) . "\n\n";
-        } else {
-            $encryptiontxt = "# Our OpenPGP key\nEncryption: $encryption\n\n";
-        }
+if (isset($encryption) && !empty($encryption)) {
+    if (is_array($encryption)){
+        $encryptiontxt = "# Our OpenPGP key\nEncryption: " . implode("\nEncryption: ", $encryption) . "\n\n";
     } else {
-        $encryptiontxt = "";
+        $encryptiontxt = "# Our OpenPGP key\nEncryption: $encryption\n\n";
     }
 } else {
     $encryptiontxt = "";
