4c4eec427b59c2905fe88aa7fe3d60e58d1a9d73
Remko <94494764+remkohat@users.noreply.github.com>
2023-02-07 13:59:08 +0100

    Update sign.php

--- securitytxt/sign/sign.php
+++ securitytxt/sign/sign.php
@@ -4,8 +4,11 @@ if (isset($publickeyfile) && !empty($publickeyfile) && file_exists($publickeyfil
     $gpg = new gnupg();
     $gpg->seterrormode(GNUPG_ERROR_WARNING);
     $gpg->setsignmode(GNUPG_SIG_MODE_CLEAR);
+    #$privatedata = file_get_contents($privatekeyfile);
     $publicdata = file_get_contents($publickeyfile);
+    #$privatekey = $gpg->import($privatedata);
     $publickey = $gpg->import($publicdata);
+    #$gpg->addsignkey($privatekey['fingerprint']);
     $gpg->addsignkey($publickey['fingerprint']);
     $signed = $gpg->sign($securitytxt);
     $securitytxt = $signed;
