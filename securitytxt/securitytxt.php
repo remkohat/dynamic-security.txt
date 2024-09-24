b385db6137b472f1bf55102975045a7ab8e2ee2b
Remko <94494764+remkohat@users.noreply.github.com>
2024-09-24 15:29:10 +0200

    Update securitytxt.php
    Redirect to HTTPS if HTTP

--- securitytxt/securitytxt.php
+++ securitytxt/securitytxt.php
@@ -1,5 +1,10 @@
 <?php
 
+if ($_SERVER['HTTPS']!="on") {
+$redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+header("Location:$redirect");
+}
+
 $headers = getallheaders();
 $host = $_SERVER['HTTP_HOST'];
 $uri = $_SERVER['REQUEST_URI'];
